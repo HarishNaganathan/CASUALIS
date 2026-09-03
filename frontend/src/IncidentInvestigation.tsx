237c2ded1190d907242f9cc30380697637172acf
K Harish Naganathan <naganathanharish907@gmail.com>
2026-09-03 23:13:23 +0530

    Fix frontend TypeScript build

--- frontend/src/IncidentInvestigation.tsx
+++ frontend/src/IncidentInvestigation.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import ReactFlow, { Background, Controls } from 'reactflow';
 import 'reactflow/dist/style.css';
 
